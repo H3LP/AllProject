968077ad5a04b1aa0a9ce8da41ac0d97121deafa
YuriYuriYur <38615205+YuriYuriYur@users.noreply.github.com>
2018-04-22 15:55:08 +0200

    flockaaaa

---
+++ Timehole/Assets/rafałCzyt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rafałCzyt : MonoBehaviour {
+	//Rafał nic z tego nie ogarnia xdxdxd
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
