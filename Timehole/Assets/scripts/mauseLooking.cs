3e10050366df7bf3735b2352c592c08d6eb1e737
YuriYuriYur <38615205+YuriYuriYur@users.noreply.github.com>
2018-04-27 20:08:02 +0200

    next

---
+++ Timehole/Assets/scripts/mauseLooking.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mauseLooking : MonoBehaviour {
+	float maximumX = -20f;
+	float minimumX = 20f;
+	float rotY;
+	float rotX;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rotY += Input.GetAxis("Mouse Y");
+		rotX += Input.GetAxis("Mouse X");
+
+		
+		
+		transform.localRotation = Quaternion.Euler(rotX, rotY, z: 0);
+
+		
+	}
+}
